cf36782cf8a10197f25ef8b4cca5115da5a5c5a2
Alana Domit Bittar <alanadomitbittar@protonmail.com>
2026-07-21 21:34:28 +0900

    fix: event list response schema
    The schema rendered in the API docs was the default DRF schema.
    Customize schema so the correct one is used.

--- app/pagination.py
+++ app/pagination.py
@@ -16,3 +16,31 @@ class EventPagination(PageNumberPagination):
                 "events": data,
             }
         )
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            "type": "object",
+            "properties": {
+                "next": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": 'http://api.example.org/accounts/?{page_query_param}=cD00ODY%3D"'.format(
+                        page_query_param=self.page_query_param
+                    ),
+                },
+                "previous": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": "http://api.example.org/accounts/?{page_query_param}=cj0xJnA9NDg3".format(
+                        page_query_param=self.page_query_param
+                    ),
+                },
+                "total": {
+                    "type": "integer",
+                    "example": 123,
+                },
+                "events": schema,
+            },
+        }
